d057ddc6c401037c09cd3ae342fb33fc3ef401f7
unknown <slaghts@gilmour.org>
2015-10-29 10:53:35 -0400

    added classes

--- pigo-slaght.py
+++ pigo-slaght.py
@@ -1,11 +1,29 @@
-#from gopigo import *
+from gopigo import *
+import time
 
 __author__ = 'Spencer'
 
 class Pigo:
 
+    isMoving = False
+    servoPos = 90
+
     def __init__(self):
         print "as you can see I am a pigo pumpkin"
 
-Moto = Pigo()
+    def stop(self):
+        self.isMoving = false
+        while stop() !=1:
+            time.sleep(.1)
+            print "I am giving at she's got captain but shit I can not stop"
 
+    def fwd(self):
+        self.ismoving = True
+        while fwd() != 1:
+            time.sleep(.1)
+            print "I am sorry but this bitch keeps ranting so I can not keep going"
+
+Moto = Pigo()
+Moto.fwd()
+Time.sleep(2)
+Moto.stop()
